42b74d96a33d15e7c12fed34fa1b2f9d0d8d1fbf
Josue Arronis <cruzjos94@gmail.com>
2019-09-11 10:44:09 -0600

    Clear state users

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,7 +6,9 @@ export class Search extends Component {
     text: ''
   };
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
   onSubmit = event => {
     event.preventDefault();
@@ -19,12 +21,18 @@ export class Search extends Component {
     });
   };
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
           <input type='text' name='text' placeholder='Search Users...' value={this.state.text} onChange={this.onChange} />
           <input type='submit' value='Search' className='btn btn-dark btn-block' />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
