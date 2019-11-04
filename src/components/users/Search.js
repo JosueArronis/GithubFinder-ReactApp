fd44a6b314419ed1476f45d49aca1c01d788fc37
Josue Arronis <30699232+JosueArronis@users.noreply.github.com>
2019-11-04 08:55:45 -0600

    Refactoring use hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,36 +1,26 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends Component {
-  state = {
-    text: ''
-  };
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
-  };
-  onSubmit = event => {
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+
+  const [text, setText] =  useState('');
+
+ const onSubmit = event => {
     event.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter something', 'light');
+    if (text === '') {
+      setAlert('Please enter something', 'light');
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: '' });
+      searchUsers(text);
+      setText( '' );
     }
   };
-  onChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
+  const onChange = event => {
+    setText( event.target.value);
   };
-  render() {
-    const { showClear, clearUsers } = this.props;
     return (
       <div>
-        <form onSubmit={this.onSubmit} className='form'>
-          <input type='text' name='text' placeholder='Search Users...' value={this.state.text} onChange={this.onChange} />
+        <form onSubmit={onSubmit} className='form'>
+          <input type='text' name='text' placeholder='Search Users...' value={text} onChange={onChange} />
           <input type='submit' value='Search' className='btn btn-dark btn-block' />
         </form>
         {showClear && (
@@ -40,7 +30,12 @@ export class Search extends Component {
         )}
       </div>
     );
-  }
+  
 }
-
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
+};
 export default Search;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,19 +1,17 @@
-import React, { Component, Fragment } from 'react';
+import React, {Fragment, useEffect} from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-  }
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
-  };
-  render() {
-    const { name, avatar_url, location, bio, company, blog, login, html_url, followers, following, public_repos, public_gists, hirable } = this.props.user;
-    const { loading } = this.props;
+import Repos  from '../repos/Repos';
+const User = ({ user, getUser, getUserRepos, loading, repos, match })  =>  {
+
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, [])
+
+    const { name, avatar_url, location, bio, company, blog, login, html_url, followers, following, public_repos, public_gists, hirable } = user;
 
     if (loading) return <Spinner />;
     return (
@@ -70,9 +68,16 @@ export class User extends Component {
           <div className='badge badge-light'>Public Repo: {public_repos}</div>
           <div className='badge badge-dark'>Public GIsts: {public_gists}</div>
         </div>
+        <Repos repos={repos}/>
       </Fragment>
     );
-  }
+  
 }
-
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired
+};
 export default User;
